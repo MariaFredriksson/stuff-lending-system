c1e6daf0e056f878dfdb5fd6d0b450cf03703a91
Maria Fredriksson <mf223wk@student.lnu.se>
2023-09-19 10:47:05 +0200

    Add basic files + basic functionality in Member

--- app/src/main/java/controller/App.java
+++ app/src/main/java/controller/App.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.Member;
+
 /**
  * Responsible for staring the application.
  */
@@ -11,10 +13,28 @@ public class App {
    */
   public static void main(String[] args) {
     // adapt to start the application in your way
-    model.Simple m = new model.Simple();
-    Simple c = new Simple();
-    view.Simple v = new view.Simple();
+    // model.Simple m = new model.Simple();
+    // Simple c = new Simple();
+    // view.Simple v = new view.Simple();
 
-    c.doSomethingSimple(m, v);
+    // c.doSomethingSimple(m, v);
+
+    // Create a new Member object
+    Member johnDoe = new Member("John Doe", "john.doe@example", 12345678, "12345678");
+
+    // Print the member's name
+    System.out.println(johnDoe.getName());
+
+    // Print the date the member was created
+    System.out.println(johnDoe.getCreationDate());
+
+    // Create another member
+    Member janeDoe = new Member("Jane Doe", "jane.doe@example", 87654321, "87654321");
+
+    // Print the member's name
+    System.out.println(janeDoe.getName());
+
+    // Print the date the member was created
+    System.out.println(janeDoe.getCreationDate());
   }
 }
---
+++ app/src/main/java/model/Member.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Member {
+  private String name;
+  private String email;
+  private int mobileNumber;
+  private String memberID;
+  private int credits;
+  private ArrayList<Item> ownedItems;
+  private Date creationDate;
+
+  public Member(String name, String email, int mobileNumber, String memberID) {
+    this.name = name;
+    this.email = email;
+    this.mobileNumber = mobileNumber;
+    this.memberID = memberID;
+
+    // Set the credits to 0 for a new member
+    this.credits = 0;
+
+    // Create an empty list of items
+    this.ownedItems = new ArrayList<Item>();
+
+    // Set the creation date to the current date
+    this.creationDate = new Date();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public int getMobileNumber() {
+    return this.mobileNumber;
+  }
+
+  public String getMemberID() {
+    return this.memberID;
+  }
+
+  public int getCredits() {
+    return this.credits;
+  }
+
+  public ArrayList<Item> getOwnedItems() {
+    return this.ownedItems;
+  }
+
+  public Date getCreationDate() {
+    return this.creationDate;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
