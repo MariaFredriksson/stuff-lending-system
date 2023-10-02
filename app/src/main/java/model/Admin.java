a7e36aaad81eede1fa46b546884813c54cc0bfc2
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-10-02 15:45:40 +0200

    Add admin to model and controller

---
+++ app/src/main/java/model/Admin.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.Random;
+
+public class Admin {
+  
+  public Admin() { 
+    
+  }
+
+  public Member createMember(String name, String email, int mobileNumber) {
+
+    String memberID = generateRandomString(10);
+
+    Member member = new Member(name, email, mobileNumber, memberID);
+
+    return member;
+
+  }
+  
+  private String generateRandomString(int length) {
+      String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      Random random = new Random();
+      StringBuilder stringBuilder = new StringBuilder(length);
+
+      for (int i = 0; i < length; i++) {
+          int randomIndex = random.nextInt(CHARACTERS.length());
+          char randomChar = CHARACTERS.charAt(randomIndex);
+          stringBuilder.append(randomChar);
+      }
+
+      return stringBuilder.toString();
+  }
+}
+
