0b5d0c7eb6062d0fdaf70d029291d28eae2552e6
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-10-05 13:26:57 +0200

    Add javadoc

--- app/src/main/java/controller/AdminController.java
+++ app/src/main/java/controller/AdminController.java
@@ -1,23 +1,46 @@
-
 package controller;
 
 import java.util.ArrayList;
-import model.Member;
 import model.AdminModel;
+import model.Member;
 import view.AdminView;
 
+/**
+ * The AdminController class is responsible for controlling interactions between the
+ * AdminModel and AdminView. It manages the creation, editing, and deletion of members,
+ * as well as displaying member information.
+ */
 public class AdminController {
-
+  /**
+   * List to store the members managed by the controller.
+   */
   private ArrayList<Member> members;
-  private AdminModel adminModel; // Create an instance of model.Admin
+
+  /**
+   * Instance of AdminModel used for member creation.
+   */
+  private AdminModel adminModel;
+
+  /**
+   * Instance of AdminView used for printing and prompting.
+   */
   private AdminView adminView;
 
+  /**
+   * Constructor to initialize the AdminController with an empty member list and instances
+   * of AdminModel and AdminView.
+   */
   public AdminController() {
     members = new ArrayList<>();
-    adminModel = new AdminModel(); // Initialize the admin instance
+    adminModel = new AdminModel();
     adminView = new AdminView();
   }
 
+  /**
+   * Prompts the user to create a new member and adds the member to the list of members.
+   *
+   * @param todaysDate The current date for member creation.
+   */
   public void createMemberPrompt(int todaysDate) {
     adminView.print("Create new member");
     String name = adminView.prompt("Enter name:");
@@ -27,6 +50,16 @@ public class AdminController {
     adminView.print("Member created!");
   }
   
+  /**
+   * Creates a new member with the given details and adds it to the list of members.
+   *
+   * @param name The name of the member.
+   * @param email The email of the member.
+   * @param mobileNumber The mobile number of the member.
+   * @param creationDate The date the member was created.
+   *
+   * @return The created member.
+   */
   public Member createMember(String name, String email, String mobileNumber, int creationDate) {
     // Validate the email and mobile number
     validateEmail(email);
@@ -37,11 +70,22 @@ public class AdminController {
     return member;
   }
   
+  /**
+   * Adds a member to the list of members.
+   *
+   * @param member The member to add.
+   */
   private void addMemberToList(Member member) {
     // Add the member to the list of members
     members.add(member);
   }
 
+  /**
+   * Returns a copy of the list of members.
+   *
+   * @return A copy of the list of members.
+   *
+   */
   public ArrayList<Member> getMembers() {
     // Return a copy of the list of members, so it is immutable
     return new ArrayList<Member>(members);
@@ -55,6 +99,12 @@ public class AdminController {
     });
   }
 
+  /**
+   * Validates that the mobile number is unique.
+   *
+   * @param mobileNumber The mobile number to validate.
+   *
+   */
   private void validateMobileNumber(String mobileNumber) {
     members.forEach(member -> {
       if (member.getMobileNumber() == mobileNumber) {
@@ -63,6 +113,9 @@ public class AdminController {
     });
   }
 
+  /**
+   * Prompts the user to edit a member and edits the member.
+   */
   public void editMemberPrompt() {
     ArrayList<Member> memberList = getMembers();
 
@@ -82,6 +135,14 @@ public class AdminController {
     adminView.print("Member edited!");
   }
 
+  /**
+   * Edits the given member with the given details.
+   *
+   * @param member The member to edit.
+   * @param name The new name of the member.
+   * @param email The new email of the member.
+   * @param mobileNumber The new mobile number of the member.
+   */
   public void editMember(Member member, String name, String email, String mobileNumber) {
     // Validate the email and mobile number
     validateEmail(email);
@@ -92,6 +153,9 @@ public class AdminController {
     member.setMobileNumber(mobileNumber);
   }
 
+  /**
+   * Prompts the user to delete a member and deletes the member.
+   */
   public void deleteMemberPrompt() {
     ArrayList<Member> memberList = getMembers();
 
@@ -104,11 +168,18 @@ public class AdminController {
     deleteMember(member);
   }
 
+  /**
+   * Deletes the given member.
+   *
+   * @param member The member to delete.
+   */
   public void deleteMember(Member member) {
-    // Delete the member
     members.remove(member);
   }
 
+  /**
+   * Prompts the user to view all members and displays the members.
+   */
   public void viewAllMembers() {
     ArrayList<Member> memberList = getMembers();
 
