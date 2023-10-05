759d78fcf04f7c97366c22f74fbe67be42955ae6
Maddelen Hedenstrom <mh222vu@student.lnu.se>
2023-10-05 14:10:07 +0200

    Add javadoc to time and member

--- app/src/main/java/model/Member.java
+++ app/src/main/java/model/Member.java
@@ -1,24 +1,35 @@
 package model;
 
 import java.util.ArrayList;
-
 import model.Item.ItemCategory;
 
+/**
+ * The Member class represents a member.
+ */
 public class Member {
   private String name;
   private String email;
   private String mobileNumber;
-  private String memberID;
+  private String memberId;
   private int credits;
   private ArrayList<Item> ownedItems;
   // private ArrayList<Item> borrowedItems;
   private int creationDate;
 
-  public Member(String name, String email, String mobileNumber, String memberID, int creationDate) {
+  /**
+   * Creates a new member.
+   *
+   * @param name The name of the member.
+   * @param email The email of the member.
+   * @param mobileNumber The mobile number of the member.
+   * @param memberId The ID of the member.
+   * @param creationDate The date when the member was created.
+   */
+  public Member(String name, String email, String mobileNumber, String memberId, int creationDate) {
     setName(name);
     setEmail(email);
     setMobileNumber(mobileNumber);
-    this.memberID = memberID;
+    this.memberId = memberId;
 
     // Set the credits to 0 for a new member
     this.credits = 0;
@@ -34,39 +45,84 @@ public class Member {
     this.creationDate = creationDate;
   }
 
+  /**
+   * Returns the name of the member.
+   *
+   * @return The name of the member.
+   */
   public String getName() {
     return this.name;
   }
 
+  /**
+   * Returns the email of the member.
+   *
+   * @return The email of the member.
+   */
   public String getEmail() {
     return this.email;
   }
 
+  /**
+   * Returns the mobile number of the member.
+   *
+   * @return The mobile number of the member.
+   */
   public String getMobileNumber() {
     return this.mobileNumber;
   }
 
-  public String getMemberID() {
-    return this.memberID;
+  /**
+   * Returns the ID of the member.
+   *
+   * @return The ID of the member.
+   */
+  public String getMemberId() {
+    return this.memberId;
   }
 
+  /**
+   * Returns the credits of the member.
+   *
+   * @return The credits of the member.
+   */
   public int getCredits() {
     return this.credits;
   }
 
+  /**
+   * Returns the list of items owned by the member.
+   *
+   * @return The list of items owned by the member.
+   */
   public ArrayList<Item> getOwnedItems() {
     // Return a copy of the list of items, so it is immutable
     return new ArrayList<Item>(this.ownedItems);
   }
 
+  /**
+   * Returns the date when the member was created.
+   *
+   * @return The date when the member was created.
+   */
   public int getCreationDate() {
     return this.creationDate;
   }
 
+  /**
+   * Sets the name of the member.
+   *
+   * @param name The name of the member.
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Sets the email of the member.
+   *
+   * @param email The email of the member.
+   */
   public void setEmail(String email) {
     if (email.contains("@") && email.contains(".")) {
       this.email = email;
@@ -76,6 +132,11 @@ public class Member {
 
   }
 
+  /**
+   * Sets the mobile number of the member.
+   *
+   * @param mobileNumber The mobile number of the member.
+   */
   public void setMobileNumber(String mobileNumber) {
     if (String.valueOf(mobileNumber).length() == 10) {
       this.mobileNumber = mobileNumber;
@@ -84,10 +145,18 @@ public class Member {
     }
   }
 
+  /**
+   * Updates the members credits.
+   *
+   * @param credits The credits to be added or subtracted.
+   */
   public void changeCredits(int credits) {
     this.credits += credits;
   }
 
+  /**
+   * Adds an item.
+   */
   public void addItem(ItemCategory category, String name, String description, int costPerDay, int todaysDate) {
     // Create a new item object
     Item newItem = new Item(category, name, description, costPerDay, this, todaysDate);
@@ -99,6 +168,11 @@ public class Member {
     this.changeCredits(100);
   }
 
+  /**
+   * Edits an item.
+   *
+   * @param item The item to be edited.
+   */
   public void editItem(Item item, ItemCategory category, String name, String description, int costPerDay) {
     // Edit the item
     item.setCategory(category);
@@ -107,6 +181,11 @@ public class Member {
     item.setCostPerDay(costPerDay);
   }
 
+  /**
+   * Deletes an item.
+   *
+   * @param item The item to be deleted.
+   */
   public void deleteItem(Item item) {
     // Delete the item from the list of items
     this.ownedItems.remove(item);
--- app/src/main/java/model/Time.java
+++ app/src/main/java/model/Time.java
@@ -1,20 +1,37 @@
 package model;
 
+/**
+ * The Time class represents the current date.
+ */
 public class Time {
   private int todaysDate;
 
+  /**
+   * Creates a new Time object.
+   */
   public Time() {
-  todaysDate = 0; 
+    todaysDate = 0; 
   }
 
+  /**
+   * Creates a new Time object.
+   */
   public int getTodaysDate() {
     return todaysDate;
   }
 
+  /**
+   * Sets the current date.
+   *
+   * @param todaysDate The current date.
+   */
   public void setTodaysDate(int todaysDate) {
     this.todaysDate = todaysDate;
   }
 
+  /**
+   * Advances the current date by one day.
+   */
   public void advanceDayCounter() {
     int day = getTodaysDate();
     setTodaysDate(day + 1);
